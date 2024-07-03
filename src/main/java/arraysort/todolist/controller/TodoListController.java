f1bcf3f3c22911ed95882c2a4d84e0343d3d05bf
arraysort <dudwns1045@gmail.com>
2024-07-03 09:30:02 +0900

    refactor : RequestMapping 으로 todo 페이지 공통 경로 제어
    1. todo 공통경로 제어 -> todo/add, todo/edit, todo/list, todo/detail
    2. 불필요 주석 제거 : 페이지 확인을 위해서 GET, POST 매핑 상관없이 GET 으로 통일

--- src/main/java/arraysort/todolist/controller/TodoListController.java
+++ src/main/java/arraysort/todolist/controller/TodoListController.java
@@ -2,34 +2,35 @@ package arraysort.todolist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 /**
- * 페이지 확인을 위해서 GET, POST 매핑 상관없이 GET 으로 통일
  * TODO : 목적에 따라서 Mapping 변경, PathVariable 사용해서 페이지 경로 조정 -> 동적 생성
  */
+@Controller
+@RequestMapping("/todo")
 public class TodoListController {
 
     // 로그인 버튼
-    @GetMapping("/todoList")
+    @GetMapping("/list")
     public String todolist() {
         return "todoList";
     }
 
     // 일정 등록 버튼
-    @GetMapping("/todoAdd")
+    @GetMapping("/add")
     public String todoAdd() {
         return "todoAdd";
     }
 
     // 일정 수정 버튼
-    @GetMapping("/todoEdit")
+    @GetMapping("/edit")
     public String todoEdit() {
         return "todoEdit";
     }
 
     // 일정 눌렀을 때
-    @GetMapping("/todoDetail")
+    @GetMapping("/detail")
     public String returnTodo() {
         return "todoDetail";
     }
