f28f3522813c15d7481d91a20244ffe41914b164
arraysort <dudwns1045@gmail.com>
2024-07-04 19:05:32 +0900

    feat : 회원가입 페이지 아이디, 패스워드, 닉네임 Blank 검증 추가
    1. 회원가입 페이지 아이디, 패스워드, 닉네임 Validation 이용 Blank 검증 추가
    2. 공백 시 입력 폼, defaultMessage 출력

--- src/main/java/arraysort/todolist/controller/SignUpController.java
+++ src/main/java/arraysort/todolist/controller/SignUpController.java
@@ -2,9 +2,12 @@ package arraysort.todolist.controller;
 
 import arraysort.todolist.domain.SignupDto;
 import arraysort.todolist.service.SignupService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +20,17 @@ public class SignUpController {
     private final SignupService signupService;
 
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(Model model) {
+        model.addAttribute("user", new SignupDto());
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String createUser(@ModelAttribute SignupDto signupDto) {
-        log.info("post : signup={}", signupDto);
+    public String createUser(@Valid @ModelAttribute("user") SignupDto signupDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
         signupService.createUser(signupDto);
         return "login";
     }
--- src/main/java/arraysort/todolist/domain/SignupDto.java
+++ src/main/java/arraysort/todolist/domain/SignupDto.java
@@ -1,16 +1,26 @@
 package arraysort.todolist.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class SignupDto {
 
+    @NotBlank(message = "아이디는 필수로 입력해야 합니다.")
     private String userId;
 
+    @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
     private String userPassword;
 
+    @NotBlank(message = "닉네임은 필수로 입력해야 합니다.")
     private String userName;
+
+    public SignupDto() {
+
+    }
 
 }
