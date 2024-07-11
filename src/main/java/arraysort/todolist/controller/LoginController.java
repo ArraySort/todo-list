310f0f9cffb1c9537dd18b4859b35b5bac40df29
arraysort <dudwns1045@gmail.com>
2024-07-11 18:57:37 +0900

    refactor : 로그인 예외처리 수정 - 유저 ID, PW 이름 수정, 컨트롤러 방식 수정
    1. login html 수정 - Spring Security 설정과 이름 일치화, signup html 과 이름 일치화
    2. Handler 에서 Controller 값 넘겨주는 방식 수정
    3. Controller 에서 thymeleaf 로 값 전달 방식 수정

--- src/main/java/arraysort/todolist/controller/LoginController.java
+++ src/main/java/arraysort/todolist/controller/LoginController.java
@@ -1,30 +1,30 @@
 package arraysort.todolist.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("login-process")
+    public String loginPrecess(HttpServletRequest request, Model model) {
+        String loginError = (String) request.getSession().getAttribute("loginError");
+        if (loginError != null) {
+            model.addAttribute("loginError", loginError);
+            request.getSession().removeAttribute("loginError");
+        }
+        return "login";
+    }
 
     @GetMapping("/")
     public String index() {
         return "redirect:/login";
     }
 
-    @PostMapping("/login-process")
-    public String loginProcess() {
-        return "login";
-    }
-
     @GetMapping("/login")
-    public String login(@RequestParam(value = "error", required = false) String error,
-                        @RequestParam(value = "exception", required = false) String exception, Model model) {
-        model.addAttribute("error", error);
-        model.addAttribute("exception", exception);
-
+    public String login() {
         return "login";
     }
 }
--- src/main/java/arraysort/todolist/security/LoginFailureHandler.java
+++ src/main/java/arraysort/todolist/security/LoginFailureHandler.java
@@ -7,29 +7,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
-public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+public class LoginFailureHandler implements AuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        String errorMessage;
 
         if (exception instanceof BadCredentialsException) {
-            errorMessage = "로그인에 실패했습니다. 아이디 또는 비밀번호를 확인해주세요";
+            request.setAttribute("loginError", "아이디 또는 비밀번호가 잘못되었습니다.");
         } else {
-            errorMessage = "알 수 없는 오류";
+            request.setAttribute("loginError", "알 수 없는 오류가 발생했습니다.");
         }
-        errorMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
-        setDefaultFailureUrl("/login?error=true&exception=" + errorMessage);
-
-        super.onAuthenticationFailure(request, response, exception);
+        request.getSession().setAttribute("loginError", "아이디 또는 비밀번호가 잘못되었습니다.");
+        response.sendRedirect("/login-process");
     }
 }
