e7d09b16c37cbabe465b517f3e1ff2611ad5cde9
arraysort <dudwns1045@gmail.com>
2024-07-16 13:24:58 +0900

    feat : 회원가입 시 닉네임 글자수, 일정 등록 시 글자수 제한 스크립트 작성
    1. 회원가입 시 닉네임에 대한 최소, 최대 글자수 제한 스크립트 작성
    2. 일정 등록 시 최소, 최대 글자수 제한 스크립트 작성
    3. 알종 등록 시 Valid 최대 글자수 수정

--- src/main/java/arraysort/todolist/domain/TodoAddDto.java
+++ src/main/java/arraysort/todolist/domain/TodoAddDto.java
@@ -13,7 +13,7 @@ public class TodoAddDto {
     @Size(min = 2, max = 20, message = "제목은 최소 2글자 이상, 최대 20글자 이하여야 합니다.")
     private String todoTitle;
 
-    @Size(max = 20, message = "내용은 최대 200자까지 입력할 수 있습니다.")
+    @Size(max = 200, message = "내용은 최대 200자까지 입력할 수 있습니다.")
     private String todoContent;
 
     @NotBlank(message = "시작 시간은 필수 입력 항목입니다.")
