622f9cc905a940d137599c6fdcee851d2577c4b2
arraysort <dudwns1045@gmail.com>
2024-07-17 15:19:32 +0900

    feat : 로그인한 유저가 로그인 페이지로 접근 시 차단 기능 추가
    1. Controller : /login 으로 request 시 Authentication 을 통해서 현재 로그인 한 유저라면 리스트의 첫 페이지로 이동하도록 리다이렉트

--- src/main/java/arraysort/todolist/controller/LoginController.java
+++ src/main/java/arraysort/todolist/controller/LoginController.java
@@ -1,18 +1,22 @@
 package arraysort.todolist.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    
+
     @GetMapping("/")
     public String index() {
         return "redirect:/login";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            return "redirect:/todo/list?page=1";
+        }
         return "login";
     }
 }
